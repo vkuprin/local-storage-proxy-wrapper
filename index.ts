6a122ea9ac29358b867fec82b4da6a57a6a003ca
Vitaly Kuprin <vitas_s@inbox.lv>
2023-01-15 22:27:49 +0100

    feat: add try catch for getter and setters

--- index.ts
+++ index.ts
@@ -4,39 +4,42 @@ export class LocalStorageWrapper {
   private changeListeners: { [key: string]: ChangeListener[] } = {};
 
   get(target: Storage, key: string) {
-    return target[key];
-  }
-
-  set(target: Storage, key: string, value: any) {
-    const oldValue = target[key];
-    target[key] = value;
-    if (this.changeListeners[key]) {
-      for (const listener of this.changeListeners[key]) {
-        listener(value, oldValue, key);
-      }
+    try {
+      return target[key];
+    } catch (e) {
+      throw new Error(`Error getting value for key ${key} from storage`);
     }
-    return true;
   }
 
-  addChangeListener(key: string, listener: ChangeListener) {
+  set(target: Storage, key: string, value: any): boolean {
+    try {
+      const oldValue = target[key];
+      target[key] = value;
+      if (this.changeListeners[key]) {
+        for (const listener of this.changeListeners[key]) {
+          listener(value, oldValue, key);
+        }
+      }
+      return true;
+    } catch (e) {
+        throw new Error(`Error setting value for key ${key} in storage`);
+    }
+  }
+
+  addChangeListener(key: string, listener: ChangeListener): void {
     if (!this.changeListeners[key]) {
       this.changeListeners[key] = [];
     }
     this.changeListeners[key].push(listener);
   }
 
-  clearChangeListeners(key: string) {
+  clearChangeListeners(key: string): void {
       if (this.changeListeners[key]) {
         this.changeListeners[key] = [];
       }
     }
 }
 
-const checkWindow: () => (object) = () => {
-  if (typeof window === 'undefined') {
-      return {};
-  }
-  return localStorage;
-}
+const checkWindow = () => (typeof window === 'undefined' ? {} : localStorage);
 
 export const localStorageProxy = new Proxy(checkWindow(), new LocalStorageWrapper());
