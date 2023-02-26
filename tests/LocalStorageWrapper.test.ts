67754974b5c4c75c6431f714161cf234068b60c6
Vitaly Kuprin <vitas_s@inbox.lv>
2023-02-26 05:17:07 +0100

    feat: more tests

--- tests/LocalStorageWrapper.test.ts
+++ tests/LocalStorageWrapper.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach } from '@jest/globals';
+import { describe, it, expect, beforeEach, jest } from '@jest/globals';
 import { LocalStorageWrapper } from "../index";
 
 
@@ -14,5 +14,60 @@ describe('LocalStorageWrapper', () => {
     it('should set a value', () => {
         wrapper.set(window.localStorage, 'test', 'test');
         expect(window.localStorage.getItem('test')).toBe('test');
+    });
+
+    it('should get a value', () => {
+        window.localStorage.setItem('test', 'test');
+        expect(wrapper.get(window.localStorage, 'test')).toBe('test');
+    });
+
+    it('should set multiple values', () => {
+        wrapper.setMultiple({ test: 'test', test2: 'test2' });
+        expect(window.localStorage.getItem('test')).toBe('test');
+        expect(window.localStorage.getItem('test2')).toBe('test2');
+    });
+
+    it('should get multiple values', () => {
+        window.localStorage.setItem('test', 'test');
+        window.localStorage.setItem('test2', 'test2');
+        expect(wrapper.getMultiple(['test', 'test2'])).toEqual({ test: 'test', test2: 'test2' });
+    });
+
+    it('should remove multiple values', () => {
+        window.localStorage.setItem('test', 'test');
+        window.localStorage.setItem('test2', 'test2');
+        wrapper.removeMultiple(['test', 'test2']);
+        expect(window.localStorage.getItem('test')).toBeNull();
+        expect(window.localStorage.getItem('test2')).toBeNull();
+    });
+
+    it('should add a change listener', () => {
+        const listener = jest.fn();
+        wrapper.addChangeListener('test', listener);
+        wrapper.set(window.localStorage, 'test', 'test');
+        expect(listener).toHaveBeenCalled();
+    })
+
+    it('should add a global change listener', () => {
+        const listener = jest.fn();
+        wrapper.addGlobalChangeListener(listener);
+        wrapper.set(window.localStorage, 'test', 'test');
+        expect(listener).toHaveBeenCalled();
+    });
+
+    it('should clear change listeners', () => {
+        const listener = jest.fn();
+        wrapper.addChangeListener('test', listener);
+        wrapper.clearChangeListeners('test');
+        wrapper.set(window.localStorage, 'test', 'test');
+        expect(listener).not.toHaveBeenCalled();
+    });
+
+    it('should clear global change listeners', () => {
+        const listener = jest.fn();
+        wrapper.addGlobalChangeListener(listener);
+        wrapper.clearGlobalChangeListeners();
+        wrapper.set(window.localStorage, 'test', 'test');
+        expect(listener).not.toHaveBeenCalled();
     });
 });
