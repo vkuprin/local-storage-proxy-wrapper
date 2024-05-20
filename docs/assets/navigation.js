b17d56205444562a6db3dc90ba0922f35d9a689f
Vitaly Kuprin <vitas_s@inbox.lv>
2024-05-20 12:31:18 +0200

    chore: new docs

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4XPsQrCMBSF4XfJXNRGRO3q2kEQdBCHa700wWsSkqu0SN/dLGqigvP5+eDs74KxY1GJ2jZAG7YeWtx5cA69KIQDVnFsCELAMP4RjRRfKJZnbU6iKuViKF7mSoFpsdaB0aQc9y5i+Zo7crKclzOZWM9uC3TFTyob/0mUnFh72/Vv7QZew5Gi+BXl6lQOhwdxtKyVPAEAAA=="
